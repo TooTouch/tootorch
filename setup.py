0796addec4bb0afe537c1b6f96b7efb74edf8441
bllfpc <wogur379@gmail.com>
2020-02-06 01:27:38 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'tootorch',
+    version             = '0.1',
+    description         = 'Implemetation XAI in Computer Vision (Pytorch)',
+    author              = 'Jaehuck Heo',
+    author_email        = 'wogur379@gmail.com',
+    url                 = 'https://github.com/TooTouch/tootorch',
+    download_url        = 'https://github.com/TooTouch/tootorch/archive/v0.1.tar.gz',
+    install_requires    =  ["torch","torchvision","h5py","tqdm","pillow","opencv-python"],
+    packages            = find_packages(exclude = []),
+    keywords            = ['tootorch','XAI'],
+    python_requires     = '>=3.6',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
