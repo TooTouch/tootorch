f02bd114fde1708e19cd9a7f0dd49c340b60ebc8
bllfpc <wogur379@gmail.com>
2020-02-06 17:20:12 +0900

    v0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name                = 'tootorch',
-    version             = '0.3',
+    version             = '0.1',
     description         = 'Implemetation XAI in Computer Vision (Pytorch)',
     author              = 'Jaehuck Heo',
     author_email        = 'wogur379@gmail.com',
