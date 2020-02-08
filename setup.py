ffdc4dfba1f2f2473010d6f3e506510e67b23fab
TooTouch <wogur379@gmail.com>
2020-02-09 04:52:32 +0900

    v0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name                = 'tootorch',
-    version             = '0.1',
+    version             = '0.2',
     long_description    = long_description,
     long_description_content_type = 'text/markdown',
     description         = 'Implemetation XAI in Computer Vision (Pytorch)',
